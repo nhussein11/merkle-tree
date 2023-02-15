2101e339537d499a0fba23966b72879039d26b10
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-02-15 16:22:33 -0300

    add: test to construct by input fn

--- src/tests.rs
+++ src/tests.rs
@@ -1,9 +1,21 @@
+
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::MerkleTree;
 
     #[test]
-    fn it_works() {
-        assert!(true);
+    fn create_two_levels_merkle_tree() {
+        let items = vec![
+            String::from("a").into_bytes(),
+            String::from("b").into_bytes(),
+            String::from("c").into_bytes(),
+            String::from("d").into_bytes(),
+            String::from("e").into_bytes(),
+        ];
+
+        let merkle_tree = MerkleTree::construct_by_input(&items);
+
+        assert_eq!(merkle_tree.levels, 3);
+        assert_eq!(merkle_tree.nodes.len(), 15); 
     }
 }
