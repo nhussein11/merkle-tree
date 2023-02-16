de2a950e9d9a57ba8b2465bcddf273bcbd452268
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-02-16 16:48:30 -0300

    add: construct by input working (wip in truth)

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,12 @@ mod tests;
 pub type Data = Vec<u8>;
 pub type Hash = Vec<u8>;
 
+#[derive(Debug)]
 pub struct MerkleTree {
     nodes: Vec<MerkleNode>,
 }
 
+#[derive(Debug, Clone)]
 pub struct MerkleNode {
     parent: Option<Rc<RefCell<MerkleNode>>>,
     hash : Hash,
@@ -29,71 +31,85 @@ impl MerkleTree {
         let input = pair_off_tree(input);
         let levels = input.len().trailing_zeros() as usize;
 
-        let mut leaves = input.iter().map(hash_data).collect::<Vec<Hash>>();
+        
+        let mut nodes : Vec<MerkleNode> = input
+                .iter()
+                .map(|leaf| MerkleNode{parent: None, hash: hash_data(leaf)})
+                .collect::<Vec<MerkleNode>>();
 
-        let mut nodes = leaves.clone();
+        let mut nodes_level = nodes.clone();
 
         for level in 0..levels {
-            leaves = leaves
-                .chunks(2)
-                .map(|chunk| concatenate_hashes(chunk[0].clone(), chunk[1].clone()))
-                .collect::<Vec<Hash>>();
+             nodes_level.chunks(2).for_each(|chunk| {
+                let mut left_child = chunk[0].clone();
+                let mut right_child = chunk[1].clone();
+            
+                let parent_hash = concatenate_and_hash(left_child.clone().hash, right_child.clone().hash);
+                let parent = MerkleNode{parent: None, hash: parent_hash};
+            
+                update_child_node(&mut left_child, parent.clone());
+                update_child_node(&mut right_child, parent.clone());
 
-            nodes.extend(leaves.clone());
+                nodes.push(parent);
+            });
+
+            nodes_level = nodes[nodes.len() - nodes_level.len() / 2..].to_vec();
         }
 
-        MerkleTree { nodes, levels }
+        println!("Nodes: {:?}", nodes);
+
+        MerkleTree { nodes }
     }
 
-    /// Check if the given data produces the given root hash
-    pub fn verify(input: &[Data], root: Hash) -> bool {
-        let merkle_tree = MerkleTree::construct_by_input(input);
-        merkle_tree.root_hash() == root
-    }
-
-    /// Returns the root hash of the MerkleTree
-    pub fn root_hash(&self) -> Hash {
-        self.nodes.last().unwrap().clone()
-    }
-
-    /// Returns the merkle proof for the given leaf index
-    pub fn get_merkle_proof_by_leaf_index(&self, index: usize) -> Result<Vec<Hash>, Error> {
-        if index >= self.get_number_of_leaves() {
-            return Err(Error::InvalidLeafIndex);
-        }
-
-        let mut proof = Vec::new();
-        let mut index = index;
-
-        for level in 0..self.levels {
-            let sibling_index = if index % 2 == 0 { index + 1 } else { index - 1 };
-
-            proof.push(self.nodes[sibling_index].clone());
-            index = index / 2;
-        }
-
-        Ok(proof)
-    }
-
-    /// Returns the merkle proof for the given data
-    pub fn get_merkle_proof_by_data(&self, data: Hash) -> Result<Vec<Hash>, Error> {
-        let data_hashed = hash_data(&data);
-        self.leaves()
-            .iter()
-            .position(|leaf| leaf == &data_hashed)
-            .map(|index| self.get_merkle_proof_by_leaf_index(index))
-            .unwrap_or(Err(Error::InvalidData))
-    }
-
-    /// Get number of leaves in the MerkleTree
-    pub fn get_number_of_leaves(&self) -> usize {
-        2i32.pow(self.levels as u32).try_into().unwrap()
-    }
-
-    /// Get leaves of the MerkleTree
-    pub fn leaves(&self) -> &[Hash] {
-        &self.nodes[0..self.get_number_of_leaves()]
-    }
+//    /// Check if the given data produces the given root hash
+//    pub fn verify(input: &[Data], root: Hash) -> bool {
+//        let merkle_tree = MerkleTree::construct_by_input(input);
+//        merkle_tree.root_hash() == root
+//    }
+//
+//    /// Returns the root hash of the MerkleTree
+//    pub fn root_hash(&self) -> Hash {
+//        self.nodes.last().unwrap().clone()
+//    }
+//
+//    /// Returns the merkle proof for the given leaf index
+//    pub fn get_merkle_proof_by_leaf_index(&self, index: usize) -> Result<Vec<Hash>, Error> {
+//        if index >= self.get_number_of_leaves() {
+//            return Err(Error::InvalidLeafIndex);
+//        }
+//
+//        let mut proof = Vec::new();
+//        let mut index = index;
+//
+//        for level in 0..self.levels {
+//            let sibling_index = if index % 2 == 0 { index + 1 } else { index - 1 };
+//
+//            proof.push(self.nodes[sibling_index].clone());
+//            index = index / 2;
+//        }
+//
+//        Ok(proof)
+//    }
+//
+//    /// Returns the merkle proof for the given data
+//    pub fn get_merkle_proof_by_data(&self, data: Hash) -> Result<Vec<Hash>, Error> {
+//        let data_hashed = hash_data(&data);
+//        self.leaves()
+//            .iter()
+//            .position(|leaf| leaf == &data_hashed)
+//            .map(|index| self.get_merkle_proof_by_leaf_index(index))
+//            .unwrap_or(Err(Error::InvalidData))
+//    }
+//
+//    /// Get number of leaves in the MerkleTree
+//    pub fn get_number_of_leaves(&self) -> usize {
+//        2i32.pow(self.levels as u32).try_into().unwrap()
+//    }
+//
+//    /// Get leaves of the MerkleTree
+//    pub fn leaves(&self) -> &[Hash] {
+//        &self.nodes[0..self.get_number_of_leaves()]
+//    }
 }
 
 /// Helper functions
@@ -121,7 +137,11 @@ fn hash_data(data: &Data) -> Hash {
     sha2::Sha256::digest(data).to_vec()
 }
 /// Concatenates the given hashes and returns the hash of the concatenated data
-fn concatenate_hashes(left: Hash, right: Hash) -> Hash {
+fn concatenate_and_hash(left: Hash, right: Hash) -> Hash {
     let parent_hash = [left, right].concat();
     hash_data(&parent_hash)
+}
+/// Update child node to point to parent
+fn update_child_node(child: &mut MerkleNode, parent: MerkleNode) {
+    child.parent = Some(Rc::new(RefCell::new(parent)));
 }
--- src/tests.rs
+++ src/tests.rs
@@ -12,127 +12,128 @@ mod tests {
         ];
 
         let merkle_tree = MerkleTree::construct_by_input(&items);
+       
 
-        assert_eq!(merkle_tree.levels, 2);
-        assert_eq!(merkle_tree.nodes.len(), 7);
+        // TODO: write correct assertions
+        assert!(!true);
     }
 
-    #[test]
-    fn create_three_levels_merkle_tree() {
-        let items = vec![
-            String::from("a").into_bytes(),
-            String::from("b").into_bytes(),
-            String::from("c").into_bytes(),
-            String::from("d").into_bytes(),
-            String::from("e").into_bytes(),
-        ];
+    //#[test]
+    //fn create_three_levels_merkle_tree() {
+    //    let items = vec![
+    //        String::from("a").into_bytes(),
+    //        String::from("b").into_bytes(),
+    //        String::from("c").into_bytes(),
+    //        String::from("d").into_bytes(),
+    //        String::from("e").into_bytes(),
+    //    ];
 
-        let merkle_tree = MerkleTree::construct_by_input(&items);
+    //    let merkle_tree = MerkleTree::construct_by_input(&items);
 
-        assert_eq!(merkle_tree.levels, 3);
-        assert_eq!(merkle_tree.nodes.len(), 15);
-    }
+    //    assert_eq!(merkle_tree.levels, 3);
+    //    assert_eq!(merkle_tree.nodes.len(), 15);
+    //}
 
-    #[test]
-    fn verify_merkle_tree() {
-        let items = vec![
-            String::from("a").into_bytes(),
-            String::from("b").into_bytes(),
-            String::from("c").into_bytes(),
-            String::from("d").into_bytes(),
-            String::from("e").into_bytes(),
-        ];
+    //#[test]
+    //fn verify_merkle_tree() {
+    //    let items = vec![
+    //        String::from("a").into_bytes(),
+    //        String::from("b").into_bytes(),
+    //        String::from("c").into_bytes(),
+    //        String::from("d").into_bytes(),
+    //        String::from("e").into_bytes(),
+    //    ];
 
-        let merkle_tree = MerkleTree::construct_by_input(&items);
+    //    let merkle_tree = MerkleTree::construct_by_input(&items);
 
-        assert!(MerkleTree::verify(&items, merkle_tree.root_hash()));
-    }
+    //    assert!(MerkleTree::verify(&items, merkle_tree.root_hash()));
+    //}
 
-    #[test]
-    fn verify_merkle_tree_should_fail() {
-        let items = vec![
-            String::from("a").into_bytes(),
-            String::from("b").into_bytes(),
-            String::from("c").into_bytes(),
-            String::from("d").into_bytes(),
-            String::from("e").into_bytes(),
-        ];
+    //#[test]
+    //fn verify_merkle_tree_should_fail() {
+    //    let items = vec![
+    //        String::from("a").into_bytes(),
+    //        String::from("b").into_bytes(),
+    //        String::from("c").into_bytes(),
+    //        String::from("d").into_bytes(),
+    //        String::from("e").into_bytes(),
+    //    ];
 
-        let merkle_tree = MerkleTree::construct_by_input(&items);
+    //    let merkle_tree = MerkleTree::construct_by_input(&items);
 
-        let mut items = items.clone();
-        items.push(String::from("f").into_bytes());
+    //    let mut items = items.clone();
+    //    items.push(String::from("f").into_bytes());
 
-        assert!(!MerkleTree::verify(&items, merkle_tree.root_hash()));
-    }
+    //    assert!(!MerkleTree::verify(&items, merkle_tree.root_hash()));
+    //}
 
-    #[test]
-    fn get_merkle_tree_proof_by_leave_index() {
-        let items = vec![
-            String::from("a").into_bytes(),
-            String::from("b").into_bytes(),
-            String::from("c").into_bytes(),
-            String::from("d").into_bytes(),
-            String::from("e").into_bytes(),
-        ];
+    //#[test]
+    //fn get_merkle_tree_proof_by_leave_index() {
+    //    let items = vec![
+    //        String::from("a").into_bytes(),
+    //        String::from("b").into_bytes(),
+    //        String::from("c").into_bytes(),
+    //        String::from("d").into_bytes(),
+    //        String::from("e").into_bytes(),
+    //    ];
 
-        let merkle_tree = MerkleTree::construct_by_input(&items);
+    //    let merkle_tree = MerkleTree::construct_by_input(&items);
 
-        let proof = merkle_tree.get_merkle_proof_by_leaf_index(0).unwrap();
+    //    let proof = merkle_tree.get_merkle_proof_by_leaf_index(0).unwrap();
 
-        assert_eq!(proof.len(), 3);
-    }
+    //    assert_eq!(proof.len(), 3);
+    //}
 
-    #[test]
-    fn get_merkle_tree_proof_by_leave_index_should_fail() {
-        let items = vec![
-            String::from("a").into_bytes(),
-            String::from("b").into_bytes(),
-            String::from("c").into_bytes(),
-            String::from("d").into_bytes(),
-            String::from("e").into_bytes(),
-        ];
+    //#[test]
+    //fn get_merkle_tree_proof_by_leave_index_should_fail() {
+    //    let items = vec![
+    //        String::from("a").into_bytes(),
+    //        String::from("b").into_bytes(),
+    //        String::from("c").into_bytes(),
+    //        String::from("d").into_bytes(),
+    //        String::from("e").into_bytes(),
+    //    ];
 
-        let merkle_tree = MerkleTree::construct_by_input(&items);
+    //    let merkle_tree = MerkleTree::construct_by_input(&items);
 
-        let proof = merkle_tree.get_merkle_proof_by_leaf_index(15);
+    //    let proof = merkle_tree.get_merkle_proof_by_leaf_index(15);
 
-        assert_eq!(proof, Err(Error::InvalidLeafIndex));
-    }
+    //    assert_eq!(proof, Err(Error::InvalidLeafIndex));
+    //}
 
-    #[test]
-    fn get_merkle_tree_proof_by_data() {
-        let items = vec![
-            String::from("a").into_bytes(),
-            String::from("b").into_bytes(),
-            String::from("c").into_bytes(),
-            String::from("d").into_bytes(),
-            String::from("e").into_bytes(),
-        ];
+    //#[test]
+    //fn get_merkle_tree_proof_by_data() {
+    //    let items = vec![
+    //        String::from("a").into_bytes(),
+    //        String::from("b").into_bytes(),
+    //        String::from("c").into_bytes(),
+    //        String::from("d").into_bytes(),
+    //        String::from("e").into_bytes(),
+    //    ];
 
-        let merkle_tree = MerkleTree::construct_by_input(&items);
+    //    let merkle_tree = MerkleTree::construct_by_input(&items);
 
-        let proof = merkle_tree
-            .get_merkle_proof_by_data(String::from("a").into_bytes())
-            .unwrap();
+    //    let proof = merkle_tree
+    //        .get_merkle_proof_by_data(String::from("a").into_bytes())
+    //        .unwrap();
 
-        assert_eq!(proof.len(), 3);
-    }
+    //    assert_eq!(proof.len(), 3);
+    //}
 
-    #[test]
-    fn get_merkle_tree_proof_by_data_should_fail() {
-        let items = vec![
-            String::from("a").into_bytes(),
-            String::from("b").into_bytes(),
-            String::from("c").into_bytes(),
-            String::from("d").into_bytes(),
-            String::from("e").into_bytes(),
-        ];
+    //#[test]
+    //fn get_merkle_tree_proof_by_data_should_fail() {
+    //    let items = vec![
+    //        String::from("a").into_bytes(),
+    //        String::from("b").into_bytes(),
+    //        String::from("c").into_bytes(),
+    //        String::from("d").into_bytes(),
+    //        String::from("e").into_bytes(),
+    //    ];
 
-        let merkle_tree = MerkleTree::construct_by_input(&items);
+    //    let merkle_tree = MerkleTree::construct_by_input(&items);
 
-        let proof = merkle_tree.get_merkle_proof_by_data(String::from("f").into_bytes());
+    //    let proof = merkle_tree.get_merkle_proof_by_data(String::from("f").into_bytes());
 
-        assert_eq!(proof, Err(Error::InvalidData));
-    }
+    //    assert_eq!(proof, Err(Error::InvalidData));
+    //}
 }
